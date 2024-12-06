157a04182d0cbb6aeb7efcd2953b4160c3c9a211
am-cid <mcidantonio@gmail.com>
2024-12-06 14:58:43 +0800

    listing: init listing model and form with migrations

--- simple_crud/listings/models.py
+++ simple_crud/listings/models.py
@@ -2,3 +2,18 @@ from django.db import models
 
 
 # Create your models here.
+class Listing(models.Model):
+    name = models.CharField(max_length=200)
+    seller = models.CharField(max_length=200)
+    seller_url = models.URLField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.name=}; {self.seller=}; {self.price=}"
+            f"{self.seller_url=}; {self.description=};"
+        )
