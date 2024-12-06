61c00774d129821322a97292c1a1f5579f785765
am-cid <mcidantonio@gmail.com>
2024-12-06 15:00:31 +0800

    listing: create, update, and delete specific listing

--- simple_crud/listings/urls.py
+++ simple_crud/listings/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.listing_list, name="listing_list"),
     path("listing/<int:pk>/", views.listing_detail, name="listing_detail"),
+    path("listing/create/", views.listing_create, name="listing_create"),
+    path("listing/<int:pk>/edit/", views.listing_update, name="listing_update"),
+    path("listing/<int:pk>/delete/", views.listing_delete, name="listing_delete"),
 ]
--- simple_crud/listings/views.py
+++ simple_crud/listings/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import get_object_or_404, render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import ListingForm
+from .models import Listing
 
 
 # Create your views here.
@@ -10,3 +14,34 @@ def listing_list(request: HttpRequest) -> HttpResponse:
 def listing_detail(request: HttpRequest, pk: int) -> HttpResponse:
     listing = get_object_or_404(Listing, pk=pk)
     return render(request, "listings/listing_detail.html", {"listing": listing})
+
+
+def listing_create(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("listing_list")
+    else:
+        form = ListingForm()
+    return render(request, "listings/listing_form.html", {"form": form})
+
+
+def listing_update(request: HttpRequest, pk: int) -> HttpResponse:
+    listing = get_object_or_404(Listing, pk=pk)
+    if request.method == "POST":
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect("listing_list")
+    else:
+        form = ListingForm(instance=listing)
+    return render(request, "listings/listing_form.html", {"form": form})
+
+
+def listing_delete(request: HttpRequest, pk: int) -> HttpResponse:
+    listing = get_object_or_404(Listing, pk=pk)
+    if request.method == "POST":
+        listing.delete()
+        return redirect("listing_list")
+    return render(request, "listings/listing_confirm_delete.html", {"listing": listing})
