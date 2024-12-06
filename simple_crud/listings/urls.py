68b9570024540ac6c2132092d91875b7a7f42cf7
am-cid <mcidantonio@gmail.com>
2024-12-06 15:00:09 +0800

    listing: list specific and all listings

--- simple_crud/listings/urls.py
+++ simple_crud/listings/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.listing_list, name="listing_list"),
+    path("listing/<int:pk>/", views.listing_detail, name="listing_detail"),
 ]
--- simple_crud/listings/views.py
+++ simple_crud/listings/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 
 # Create your views here.
+def listing_list(request: HttpRequest) -> HttpResponse:
+    listings = Listing.objects.all()
+    return render(request, "listings/listing_list.html", {"listings": listings})
+
+
+def listing_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    listing = get_object_or_404(Listing, pk=pk)
+    return render(request, "listings/listing_detail.html", {"listing": listing})
