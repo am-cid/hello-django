eb897a5db94a1c9435194278676dcf2a9969d081
am-cid <mcidantonio@gmail.com>
2024-12-11 23:46:03 +0800

    forms,templates: update listing form, detail template, and delete template to have listing image

--- simple_crud/listings/forms.py
+++ simple_crud/listings/forms.py
@@ -5,9 +5,18 @@ from .models import Listing, Profile
 
 
 class ListingForm(forms.ModelForm):
+    picture = forms.ImageField(
+        label="Listing Photo", required=False, widget=forms.FileInput
+    )
     class Meta:
         model = Listing
-        fields = ["name", "seller_url", "description", "price"]
+        fields = [
+            "name",
+            "seller_url",
+            "description",
+            "price",
+            "picture",
+        ]
 
 
 class UserRegistrationForm(forms.ModelForm):
