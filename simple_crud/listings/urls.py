0305b080cca21cea50dc3e001d506d332b555469
Naxus <98635973+SpottedYodeler2@users.noreply.github.com>
2024-12-17 03:07:04 +0800

    feat/Confirm Delete Product and Confirm Logout
    - modal for confirm logout
    - page confirmation when user wants to delete listing

--- simple_crud/listings/urls.py
+++ simple_crud/listings/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.welcome, name="welcome"),
+    path("welcome", views.welcome, name="welcome"),
     path("home/", views.home, name="home"),
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
